6ea87e41938ead9c5b611001d7e0ce22610f1aaf
oopanpan <oopanpan@gmail.com>
2021-03-24 16:54:56 -0700

    commit tree 8132bbdb31924aa911770660304c1b5ecda3aed3

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
+  const renderActors = () => (
+    actors.map(actor => (
+      <div>
+        <h3>{actor.name}</h3>
+        <ul>
+          {actor.movies.map(movie => <li>{movie}</li>)}
+        </ul>
+      </div>
+    ))
+  )
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors()}
     </div>
   );
 };
